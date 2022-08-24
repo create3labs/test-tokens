32c2a631c9b50c62460358ec1d9294809108547e
seb0zz <sebastian@autobahn.network>
2022-08-24 19:13:08 +0200

    Set node version and default value for etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,7 @@ const config: HardhatUserConfig = {
     currency: "USD",
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.ETHERSCAN_API_KEY || "abc",
   },
 };
 
